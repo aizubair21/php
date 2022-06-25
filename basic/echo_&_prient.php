323e4c02a4a828fae227b7a8dc01ed3bafa72b55
aizubair21 <amimlit21@gmail.com>
2022-06-25 22:31:03 +0600

    different between echo and prient

---
+++ basic/echo_&_prient.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * first thing in all programming language is show anything into console or page.
+ * php print al thing into webpage.
+ * 
+ * php have many function to show your result into webpage. as,
+ * echo()
+ * print_r()
+ * var_dump()
+ * printr()
+ * print()
+ * die()
+ * etc
+ * 
+ * they are so called language constructors.
+ * now we see echo and prient
+ */
+
+/**
+ * different between echo and prient
+ * 
+ * first thing, echo can only prient single value is string, or int. only value.
+ * 
+ */
+
+echo 'In php you can use either echo statement, <br>';
+print 'or the prient statement, To show output text';
+
+//You can combine multiple string by using a '.'(dot)
+
+$str1 = 'You can combine';
+$str2 = $str1 . ' multiple string together';
+
+echo '<br></br>';
+echo $str2;
+
+// you can user '' single or "" double quote in echo. but here a simple different.
+// double quote tage a variable, but single quote does not take. 
+// single quote make a variable into plain string.
+
+echo "<br></br>";
+echo 'You can use single quote <br>';
+echo "or double quote <br></br>";
+
+$name = 'lorem';
+echo $name . ' is a variable <br>';
+echo 'but, single quote not take a $name <br>'; //heare $name a string.
+
+echo "but, double quote take a $name"; //here $name a variable.
+
+
+
+/**
+ * echo always print a single value. can't print an array, or <object data="" type="" class=""></object>
+ */
+
+$fruits = ['apple', 'banana', 'orange', 'water mellon', 'mango'];
+echo $fruits . '<br>'; // give you an error
+
+/**
+ * but echo can print s item from array
+ */
+
+echo $fruits[2]; //echo 'orange';
---
+++ basic/getting_started.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Getting started with php.
+ * 
+ * As a server based language. To practise php you have an server.
+ * lovely you don't need any web server.
+ * XAMPP is for you. XAMPP give you the server invironment.
+ * 
+ * So, First thing you have to download a development enviroment of XAMPP for a server plateform.
+ * for windows use XAMPP, for linux use LAMPP.
+ * see Apaceh Friends document for instruction how to install xmpp or lampp
+ * 
+ * after successfully isntalled xampp. you need a text aditor for edition your code. 
+ * i suggest you can use VsCode or Sublime text editor. 
+ * 
+ * after install necesary things. run your XMPP from xmpp control panel.
+ * and visit localhost by web browser.
+ * in xampp directory you can see a derectory named "htdocs". it's your server file. all of your code inside heare.
+ * 
+ */
