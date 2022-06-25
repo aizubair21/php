2af783f1205605d44bbf76b368beb7322c9e936a
aizubair21 <amimlit21@gmail.com>
2022-06-25 22:02:38 +0600

    start code

---
+++ basic/essential.php
@@ -0,0 +1,34 @@
+<?php
+/*
+
+--------------- About PHP ----------------
+PHP is an acronym for Hypertext Preprocessor language.
+
+It is an open-source purpose scripting language, which is used to create dynamic websites.
+
+PHP is cross plateform language, Whick means it can run on Windows, Linux, ans Unix OS. and support different database like MySql, MS Access, Oracle etc.
+
+
+--------------Why I love php--------------
+1-> first thing i love php as it's cross plateform, because it is open source.
+2->Another reason of its properly support most web servers.
+3-> Compared to another programming language the learning curve of PHP is not so long.
+4-> We have huge community for support us.
+5-> It intregate into HTML is very easy.
+6-> Have built in web server.
+7-> PHP is integrated with a different database, MySql.
+8-> Not need to decler data type in php. it's more favourite.
+9-> In php 8 have a feature name JIT. Just-In-Time Compile. JIT is the popular fiture in Javascript V8 engine.
+
+
+
+---------------- Basic Usage of php -----------
+1-> php is used into major plateform like wordpress.
+2-> One of the biggest uses of php is design Server Side Application.
+3-> It's easy to intregate with database.
+4-> It's Open Source. So, have large number of community support.
+
+Now We are using php 8.0 but here we practises 7.4. but, we have JIT in php 8.
+
+
+*/
